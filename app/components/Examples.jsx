8c0f07ff4d75fa20ff9eb8389f2fe751be89a07c
Trong Nguyen <trongn1985@gmail.com>
2017-06-02 22:52:49 -0700

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -3,7 +3,10 @@ var React = require('react');
 var Examples= React.createClass({
   render: function() {
     return(
-      <h2>Examples Component!</h2>
+      <div>
+        <h2>Examples Component!</h2>
+        <p>Welcome to examples page!</p>
+      </div>
     );
   }
 });
